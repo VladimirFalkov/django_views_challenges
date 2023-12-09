3a986a4528695c9a4dd919771e667172a216c6dc
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-12-09 16:41:54 +0300

    happy with level 2 authorization

--- django_views_routing_homework/views/level_2/d_authorization.py
+++ django_views_routing_homework/views/level_2/d_authorization.py
@@ -22,28 +22,32 @@ process_authorization_view - обрабатывает заполненные д�
     5. На странице вы увидете сообщение об успехе или неудаче.
 """
 USERNAME_TO_PASSWORD_MAPPER = {
-    'john_doe': 'password123',
-    'sarah_connor': 'terminator2',
-    'admin': 'admin_pass',
-    'coder2021': 'qwerty',
-    'happy_user': '12345',
-    'l33t_h4ck3r': 'leetpassword',
-    'music_lover': 'beethoven',
-    'sports_fan': 'goal2023',
-    'travel_guru': 'wanderlust',
+    "john_doe": "password123",
+    "sarah_connor": "terminator2",
+    "admin": "admin_pass",
+    "coder2021": "qwerty",
+    "happy_user": "12345",
+    "l33t_h4ck3r": "leetpassword",
+    "music_lover": "beethoven",
+    "sports_fan": "goal2023",
+    "travel_guru": "wanderlust",
 }
 
 
 @csrf_exempt
 def process_authorization_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
-        # код писать тут
+        username = data.get("username")
+        password = data.get("password")
+        if username in USERNAME_TO_PASSWORD_MAPPER and password == USERNAME_TO_PASSWORD_MAPPER[username]:
+            return JsonResponse(data={}, status=200)
+        else:
+            return JsonResponse(data={}, status=403)
     else:
-        return HttpResponseNotAllowed(permitted_methods=['POST'])
+        return HttpResponseNotAllowed(permitted_methods=["POST"])
 
 
 # не обращайте внимания на эту вьюху, она нужна лишь для отрисовки страницы авторизации
 def authorization_view(request):
-    return render(request, 'authorization.html')
-
+    return render(request, "authorization.html")
